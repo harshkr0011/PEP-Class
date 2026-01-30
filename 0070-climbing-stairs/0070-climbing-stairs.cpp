129ad680ac9d8bffd40b8e2ad87f6e755fe400f8
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2026-01-30 16:16:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,22 @@
-// class Solution {
-// public:
-//     int climbStairs(int n) {
-//         int a = 1, b = 1;
-//         for(int i = 2; i <= n; i++){
-//             int c = a + b;
-//             a = b;
-//             b = c;
-//         }
-//         return b;
-//     }
-// };
-
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 1)
-            return 1;
-
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        int a = 1, b = 1;
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
     }
 };
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n <= 1)
+//             return 1;
+
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// };
