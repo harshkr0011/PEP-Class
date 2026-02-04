e028c1381cdb04a1335c69ec1cb64a303f868958
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2026-02-04 14:51:21 +0530

    Time: 0 ms (100%), Space: 16.1 MB (67.28%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -1,32 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-
- 
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL || head->next==NULL){
+
+        // if list is empty and has only one elements than no duplicate is possible 
+        //so return the head 
+        if(head == NULL || head -> next == NULL){
             return head;
         }
+
         ListNode* temp1 = head;
-        ListNode* temp2 = head->next;
-        while(temp2!=NULL){
-            if(temp1->val==temp2->val){
-                temp2=temp2->next;
-                temp1->next = temp2;
+        ListNode* temp2 = head -> next;
+        while(temp2 != NULL){
+            if(temp1 -> val == temp2 -> val){
+                temp2 = temp2 -> next;
+                temp1 -> next = temp2;
             }
             else{
-                temp1=temp1->next;
-                temp2=temp2->next;
+                temp1 = temp1 -> next;
+                temp2 = temp2 -> next;
             }
         }
         return head;
