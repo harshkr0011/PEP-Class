b3b2732e2f2da383fb0bf86fe8dc64e9f86553b2
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2026-02-09 16:25:55 +0530

    Time: 0 ms (100%), Space: 8.9 MB (11.77%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,12 +1,30 @@
-bool wordPattern(string pattern, string str) {
-    map<char, int> p2i;
-    map<string, int> w2i;
-    istringstream in(str);
-    int i = 0, n = pattern.size();
-    for (string word; in >> word; ++i) {
-        if (i == n || p2i[pattern[i]] != w2i[word])
-            return false;
-        p2i[pattern[i]] = w2i[word] = i + 1;
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        int m = s.size();
+        int n = pattern.size();
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        while(ss >> word){
+            words.push_back(word);
+        }
+
+        // for(auto &w: words){
+        //     cout<<w<<" ";
+        // }
+        if(words.size() != pattern.size()) return false;
+        unordered_map<char,string>mp1;
+        unordered_map<string, char>mp2;
+        
+        for(int i =0; i<n; i++){
+            char ch1 = pattern[i];
+            string ch2 = words[i];
+            if(mp1.find(ch1)!=mp1.end() && mp1[ch1]!=ch2 || mp2.find(ch2)!=mp2.end() && mp2[ch2] != ch1 ) return false;
+
+            mp1[ch1] = ch2;
+            mp2[ch2] = ch1;
+        }
+        return true;
     }
-    return i == n;
-}
+};
