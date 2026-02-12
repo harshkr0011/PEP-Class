99af9c5696e74dff5eda57c2e93bd579b90d0587
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2026-02-12 13:17:09 +0530

    Time: 0 ms (100%), Space: 21.6 MB (77.36%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        std::stack<int> stack;
+        vector<int> st; // Using vector as a stack for easier output
 
-        for (int a : asteroids) {
-            if (a > 0) {
-                stack.push(a);
-            } else {
-                while (!stack.empty() && stack.top() > 0 && stack.top() < -a) {
-                    stack.pop();
-                }
+        for (int curr : asteroids) {
+            bool exploded = false;
 
-                if (stack.empty() || stack.top() < 0) {
-                    stack.push(a);
+            // Collision only happens if:
+            // 1. Stack is not empty
+            // 2. Top of stack is moving RIGHT (+)
+            // 3. Current asteroid is moving LEFT (-)
+            while (!st.empty() && st.back() > 0 && curr < 0) {
+                
+                if (abs(st.back()) < abs(curr)) {
+                    // Top of stack is smaller: it explodes, keep checking
+                    st.pop_back();
+                    continue; 
+                } 
+                else if (abs(st.back()) == abs(curr)) {
+                    // Both are same size: both explode
+                    st.pop_back();
+                    exploded = true;
+                    break;
+                } 
+                else {
+                    // Current is smaller: current explodes
+                    exploded = true;
+                    break;
                 }
+            }
 
-                if (!stack.empty() && stack.top() == -a) {
-                    stack.pop();
-                }
+            if (!exploded) {
+                st.push_back(curr);
             }
         }
 
-        std::vector<int> res(stack.size());
-        int i = stack.size() - 1;
-
-        while (!stack.empty()) {
-            res[i--] = stack.top();
-            stack.pop();
-        }
-
-        return res;        
+        return st;
     }
 };
